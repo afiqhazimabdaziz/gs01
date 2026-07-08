1aa38e6ef8ebfaa0634215fe7a7a181d1386118c
afiqhazimabdaziz <afiqhazimabdaziz@gmail.com>
2026-07-08 22:57:34 +0800

    use relative paths for production

--- vite.config.js
+++ vite.config.js
@@ -11,15 +11,25 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    base: process.env.APP_URL || '/',
+    // Use relative paths for production
+    base: process.env.APP_ENV === 'production' ? '/build/' : '/',
     build: {
         manifest: true,
         outDir: 'public/build',
         emptyOutDir: true,
         rollupOptions: {
             output: {
-                assetFileNames: '[name]-[hash][extname]',
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                chunkFileNames: 'js/[name]-[hash].js',
+                entryFileNames: 'js/[name]-[hash].js',
             },
+        },
+    },
+    server: {
+        https: false, // Set to false for production
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
         },
     },
 });
